f39c22002328e274c0ce3e504b66af19d184751b
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-02-01 12:21:19 +0600

    structure the project layout. and add some section of header like navbar and info

---
+++ header.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Makfare</title>
+    <!-- fav icon link  -->
+    <link rel="shortcut icon" href="./assets/makfare_logo.png" type="image/x-icon">
+    <!-- style sheet link  -->
+    <link rel="stylesheet" href="./styles/header.css">
+</head>
+
+<body>
+    <header class="container">
+        <!-- brand heading section start -->
+        <div class="branding mf-flex-center">
+            <div class="brand">
+                <figure class="mf-flex-center">
+                    <img src="./assets/makfare_logo.png" alt="brand logo">
+                </figure>
+            </div>
+
+            <!-- contact information section -->
+            <div class="brand-information mf-flex">
+                <!-- social icons  -->
+                <ul class="social-icons">
+                    <li class="icon-item">
+                        <a class="icon-link" href="">
+                            <i class="fa-brands fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li class="icon-item">
+                        <a class="icon-link" href="">
+                            <i class="fa-brands fa-facebook"></i>
+                        </a>
+                    </li>
+                    <li class="icon-item">
+                        <a class="icon-link" href="">
+                            <i class="fa-brands fa-pinterest-p"></i>
+                        </a>
+                    </li>
+                    <li class="icon-item">
+                        <a class="icon-link" href="">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+                    </li>
+                </ul>
+
+                <!-- phone info  -->
+                <div class="info-container">
+                    <!-- icon  -->
+                    <figure class="info-icon">
+                        <img src="./assets/phone_icon.png" alt="phone icon">
+
+                    </figure>
+                    <!-- phone number -->
+                    <div class="info-text-container">
+                        <p class="info-sub-text">call anytime</p>
+                        <p class="info-text">+8801335-101449</p>
+                    </div>
+                </div>
+
+                <!-- mail info  -->
+                <div class="info-container">
+                    <!-- icon  -->
+                    <figure class="info-icon">
+                        <img src="./assets/mail_icon.png" alt="mail icon">
+
+                    </figure>
+                    <!-- mail address -->
+                    <div class="info-text-container">
+                        <p class="info-sub-text">call anytime</p>
+                        <p class="info-text">+8801335-101449</p>
+                    </div>
+                </div>
+
+                <!-- location info  -->
+                <div class="info-container">
+                    <!-- icon  -->
+                    <figure class="info-icon">
+                        <img src="./assets/location_icon.png" alt="location icon">
+
+                    </figure>
+                    <!-- location -->
+                    <div class="info-text-container">
+                        <p class="info-sub-text">call anytime</p>
+                        <p class="info-text">+8801335-101449</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- brand heading section end -->
+
+
+        <!-- navbar section start  -->
+        <nav class="mf-flex">
+            <!-- navigation menu  -->
+            <ul class="navbar mf-flex">
+                <li class="nav-item"><a href="" class="nav-link">Home</a></li>
+                <li class="nav-item"><a href="" class="nav-link">Services</a></li>
+                <li class="nav-item"><a href="" class="nav-link">Blog</a></li>
+                <li class="nav-item"><a href="" class="nav-link">Tissue Culture</a></li>
+                <li class="nav-item"><a href="" class="nav-link">Blog</a></li>
+                <li class="nav-item"><a href="" class="nav-link">About</a></li>
+            </ul>
+
+            <!-- CTA buttons  -->
+            <div>
+                <button type="button">Contact Us</button>
+            </div>
+        </nav>
+        <!-- navbar section end  -->
+    </header>
+
+
+    <!-- font awesome cdn  -->
+    <script src="https://kit.fontawesome.com/ff87b718c4.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Makfare</title>
+    <!-- favicon link  -->
+    <link rel="shortcut icon" href="./assets/makfare_logo.png" type="image/x-icon">
+
+    <!-- style sheet link  -->
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+
+<body>
+    <!-- header section  start-->
+    <header>
+        <h1>Header section of Macfare</h1>
+    </header>
+    <!-- header section end  -->
+
+    <!-- main section start  -->
+    <main>
+        <p>main section</p>
+    </main>
+    <!-- main section end -->
+
+    <!-- footer section start  -->
+    <footer>
+        <h3>Footer section</h3>
+    </footer>
+    <!-- footer section end  -->
+</body>
+
+</html>
